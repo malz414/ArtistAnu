ab5ea39e164bde389f986ac02bb9a1f0a34cad4f
malz414 <72981910+malz414@users.noreply.github.com>
2025-02-23 13:55:49 +0000

    no celery

--- craft_catalogue/craft_catalogue/settings.py
+++ craft_catalogue/craft_catalogue/settings.py
@@ -116,5 +116,3 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
